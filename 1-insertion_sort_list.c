755bb4b20669d2b67a19a6593932108547d3a879
cristian <cristaker1927@hotmail.com>
2022-02-14 14:57:16 -0800

    point 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,17 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - function that sorts a doubly linked lis
+ * @list: doubly linked list
+ * Return: no return
+ * **/
+
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *ptr, *tmp;
 
+	if (!list)
+		return;
+
+	ptr = *list;
 }
